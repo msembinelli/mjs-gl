c7b206cb6d36bdc2ffcc1c1729b5e3ae942834f5
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-02-24 14:34:19 -0700

    Added tess_eval shader code to draw 2 triangle lines

--- CurvesAndSplines/shaders/tess_eval.glsl
+++ CurvesAndSplines/shaders/tess_eval.glsl
@@ -4,10 +4,27 @@
 // to be passed to the fragment shader.
 
 //We want to output lines aka an isoline
+layout(isolines) in;
 
 void main( )
 {
     // receive original positions
+    vec4 p0 = gl_in[0].gl_Position;
+    vec4 p1 = gl_in[1].gl_Position;
+    vec4 p2 = gl_in[2].gl_Position;
 
     // set position of new vertex
+    float u = gl_TessCoord.x;
+    if(u < 0.334)
+    {
+        gl_Position = p0;
+    }
+    else if(u > 0.667)
+    {
+        gl_Position = p2;
+    }
+    else
+    {
+        gl_Position = p1;
+    }
 }
