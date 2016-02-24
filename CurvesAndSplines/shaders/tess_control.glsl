5381da5af4fb68885cfc119d465aa3f0a2558f2f
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-02-24 14:26:01 -0700

    Added control shader code

--- CurvesAndSplines/shaders/tess_control.glsl
+++ CurvesAndSplines/shaders/tess_control.glsl
@@ -1,14 +1,17 @@
 #version 430 core
 // Tessellation Control Shader runs once per input vertex.
 // It tells the Tessellator how many vertices to create
-
+layout (vertices = 3) out;
 // output patches with a particular vertex count.
 
 void main( )
 {
   //pass the incoming vertex position down the pipeline
+  gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 
   // Since we are dealing with isolines, the following tessellation parameters
   // need to be set. If you are tessellating triangles, etc these parameters will
   // have different meanings.
+  gl_TessLevelOuter[0] = 1;
+  gl_TessLevelOuter[1] = 2;
 }
