9ddd89cd4de1dfda2458ea918d78d8b98be80871
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-02-28 13:27:00 -0700

    Added multisampling to the window for better appearance

--- CurvesAndSplines/boilerplate.cpp
+++ CurvesAndSplines/boilerplate.cpp
@@ -412,6 +412,7 @@ int main(int argc, char *argv[])
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     window = glfwCreateWindow(512, 512, "CPSC 453 OpenGL Boilerplate", 0, 0);
     if (!window) {
         cout << "Program failed to create GLFW window, TERMINATING" << endl;
