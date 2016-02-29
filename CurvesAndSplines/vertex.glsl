e0eb43996fc2dab278607c011e308a6f696f288d
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-02-29 14:13:38 -0700

    Changed colours for control lines

--- CurvesAndSplines/vertex.glsl
+++ CurvesAndSplines/vertex.glsl
@@ -41,6 +41,6 @@ void main()
             Colour = VertexColour;
         }
         else if(control_lines)
-            Colour = vec3(0.3, 0.8, 0.6);
+            Colour = vec3(0.1, 0.5, 0.1);
     }
 }
