3ed0338a19ff3fe92a9ce7673ee4e313fc6cdc24
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-03-16 14:23:55 -0600

    Removed unneeded geometry code

--- RayTracing/boilerplate.cpp
+++ RayTracing/boilerplate.cpp
@@ -81,108 +81,6 @@ void DestroyShaders(MyShader *shader)
 }
 
 // --------------------------------------------------------------------------
-// Functions to set up OpenGL buffers for storing geometry data
-
-struct MyGeometry
-{
-    // OpenGL names for array buffer objects, vertex array object
-    GLuint  vertexBuffer;
-    GLuint  colourBuffer;
-    GLuint  vertexArray;
-    GLsizei elementCount;
-
-    // initialize object names to zero (OpenGL reserved value)
-    MyGeometry() : vertexBuffer(0), colourBuffer(0), vertexArray(0), elementCount(0)
-    {}
-};
-
-// create buffers and fill with geometry data, returning true if successful
-bool InitializeGeometry(MyGeometry *geometry)
-{
-    // three vertex positions and assocated colours of a triangle
-    const GLfloat vertices[][2] = {
-        { -0.6, -0.4 },
-        {  0.6, -0.4 },
-        {  0.0,  0.6 }
-    };
-    const GLfloat colours[][3] = {
-        { 1.0, 0.0, 0.0 },
-        { 0.0, 1.0, 0.0 },
-        { 0.0, 0.0, 1.0 }
-    };
-    geometry->elementCount = 3;
-
-    // these vertex attribute indices correspond to those specified for the
-    // input variables in the vertex shader
-    const GLuint VERTEX_INDEX = 0;
-    const GLuint COLOUR_INDEX = 1;
-
-    // create an array buffer object for storing our vertices
-    glGenBuffers(1, &geometry->vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, geometry->vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // create another one for storing our colours
-    glGenBuffers(1, &geometry->colourBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, geometry->colourBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
-
-    // create a vertex array object encapsulating all our vertex attributes
-    glGenVertexArrays(1, &geometry->vertexArray);
-    glBindVertexArray(geometry->vertexArray);
-
-    // associate the position array with the vertex array object
-    glBindBuffer(GL_ARRAY_BUFFER, geometry->vertexBuffer);
-    glVertexAttribPointer(VERTEX_INDEX, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(VERTEX_INDEX);
-
-    // assocaite the colour array with the vertex array object
-    glBindBuffer(GL_ARRAY_BUFFER, geometry->colourBuffer);
-    glVertexAttribPointer(COLOUR_INDEX, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(COLOUR_INDEX);
-
-    // unbind our buffers, resetting to default state
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    // check for OpenGL errors and return false if error occurred
-    return !CheckGLErrors();
-}
-
-// deallocate geometry-related objects
-void DestroyGeometry(MyGeometry *geometry)
-{
-    // unbind and destroy our vertex array object and associated buffers
-    glBindVertexArray(0);
-    glDeleteVertexArrays(1, &geometry->vertexArray);
-    glDeleteBuffers(1, &geometry->vertexBuffer);
-    glDeleteBuffers(1, &geometry->colourBuffer);
-}
-
-// --------------------------------------------------------------------------
-// Rendering function that draws our scene to the frame buffer
-
-void RenderScene(MyGeometry *geometry, MyShader *shader)
-{
-    // clear screen to a dark grey colour
-    glClearColor(0.2, 0.2, 0.2, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // bind our shader program and the vertex array object containing our
-    // scene geometry, then tell OpenGL to draw our geometry
-    glUseProgram(shader->program);
-    glBindVertexArray(geometry->vertexArray);
-    glDrawArrays(GL_TRIANGLES, 0, geometry->elementCount);
-
-    // reset state to default (no shader or geometry bound)
-    glBindVertexArray(0);
-    glUseProgram(0);
-
-    // check for an report any OpenGL errors
-    CheckGLErrors();
-}
-
-// --------------------------------------------------------------------------
 // GLFW callback functions
 
 // reports GLFW errors
@@ -239,11 +137,6 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    // call function to create and fill buffers with geometry data
-    MyGeometry geometry;
-    if (!InitializeGeometry(&geometry))
-        cout << "Program failed to intialize geometry!" << endl;
-
     ImageBuffer image;
     image.Initialize();
 
@@ -251,7 +144,6 @@ int main(int argc, char *argv[])
     while (!glfwWindowShouldClose(window))
     {
         // call function to draw our scene
-        //RenderScene(&geometry, &shader);
         image.Render();
 
         // scene is rendered to the back buffer, so swap to front for display
@@ -262,7 +154,6 @@ int main(int argc, char *argv[])
     }
 
     // clean up allocated resources before exit
-    DestroyGeometry(&geometry);
     DestroyShaders(&shader);
     glfwDestroyWindow(window);
     glfwTerminate();
